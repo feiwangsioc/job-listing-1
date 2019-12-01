8c79f77abbcdcf407ca8dacc69b0ee2d519216a9
feiwang <feiwangsioc@gmail.com>
2019-12-01 01:31:26 +0000

    implement wage part

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -46,8 +46,8 @@ class JobsController < ApplicationController
   end 
   
   private
-  def job_params
-    params.require(:job).permit(:title, :description)
-  end 
   
+  def job_params
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+  end
 end
